ccfc57b56dd7fe3f6e353f94544e26c164a49312
juandie019 <jdiego.gomez@alumnos.udg.mx>
2021-07-13 15:06:27 -0500

    image_base_url removed

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useParams  } from 'react-router-dom';
 //config
-import { IMAGE_BASE_URL, Image_Base_URL, POSTER_SIZE } from '../config';
+import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
 
 //components;
 import Grid from './Grid';
